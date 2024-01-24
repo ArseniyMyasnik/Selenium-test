06ce0bc1018eca7e691e7fbc867f6c0ff9d8077e
Arseniy Myasnik <arseniym148@gmail.com>
2024-01-24 21:15:19 +0500

    Added assert

--- test_items.py
+++ test_items.py
@@ -6,5 +6,8 @@ from selenium.webdriver.common.by import By
 def test_add_to_bin(browser):
     browser.implicitly_wait(5)
     element = browser.find_element(By.CLASS_NAME, "btn-add-to-basket")
+    is_button_enable = element.is_enabled()
+    assert is_button_enable == True
     element.click()
-    
+    browser.implicitly_wait(5)
+    browser.execute_script('window.scrollBy(0, 300);')
