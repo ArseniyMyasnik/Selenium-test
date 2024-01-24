bce6ff5744affdb2c573810d633a5862e6bdfa2f
Arseniy Myasnik <arseniym148@gmail.com>
2024-01-24 21:09:45 +0500

    Added assert

--- test_items.py
+++ test_items.py
@@ -7,3 +7,5 @@ def test_add_to_bin(browser):
     browser.implicitly_wait(5)
     element = browser.find_element(By.CLASS_NAME, "btn-add-to-basket")
     element.click()
+    is_button_enable = element.is_enabled()
+    assert is_button_enable == True
